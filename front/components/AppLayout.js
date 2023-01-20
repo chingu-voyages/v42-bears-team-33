2efb09db717f6b755574fbe58177a515eb9a342b
mirrer <alsejr1004@gmail.com>
2023-01-20 14:11:38 +0900

    Tag: Add layout header

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Row, Button } from 'antd';
 import PropTypes from 'prop-types';
 
 const AppLayout = ({ children }) => {
   return (
-    <div>
-      <div>Common Menu</div>
+    <>
+      <Row>
+        <Row>
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/000/567/055/small/vector60-2428-01.jpg"
+            alt="home logo"
+          />
+          <header>Friendship App</header>
+        </Row>
+
+        <Row>
+          <Button type="primary" size="large">
+            Log in
+          </Button>
+          <Button size="large">Sign up</Button>
+        </Row>
+      </Row>
       {children}
-    </div>
+    </>
   );
 };
 
