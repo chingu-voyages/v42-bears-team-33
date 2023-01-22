373f4d38b6b3698183c27bafc0e3044301f2ea66
mirrer <alsejr1004@gmail.com>
2023-01-22 20:13:40 +0900

    Feat: Login, Signup page head

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Head from 'next/head';
 
 import AppLayout from '@components/AppLayout';
 
 const Signup = () => {
   return (
-    <AppLayout>
-      <div>Signup Page</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>Sign Up | Friendship Maintainer</title>
+      </Head>
+
+      <AppLayout>
+        <div>Signup Page</div>
+      </AppLayout>
+    </>
   );
 };
 
