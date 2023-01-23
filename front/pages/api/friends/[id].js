a0e01d48ddea9b20d71167f0528e27a1d2a08a44
Nazri <649000@users.noreply.github.com>
2023-01-23 13:10:29 +0800

    Added implementation to findOne, delete and update /api/friends/{id}

--- front/pages/api/friends/[id].js
+++ front/pages/api/friends/[id].js
@@ -1,10 +1,12 @@
 import { ObjectId } from 'mongodb';
+import pino from 'pino';
 import clientPromise from '../../../config/mongodb';
 import {
-  HTTP, HTTP_STATUS_CODE, MONGODB_COLLECTION, MONGODB_DATABASE
+  HTTP,
+  HTTP_STATUS_CODE,
+  MONGODB_COLLECTION,
+  MONGODB_DATABASE,
 } from '../../../config/constant';
-
-import pino from 'pino';
 
 export default async function handler(req, res) {
   // TODO
@@ -18,34 +20,53 @@ export default async function handler(req, res) {
 
   switch (method) {
     case HTTP.GET: {
-      const allFriends = await db
-        .collection(MONGODB_COLLECTION.FRIEND)
-        .find({})
-        .toArray();
-      res.json({ status: HTTP_STATUS_CODE.OK, data: allFriends });
+      logger.info(`HTTP GET: /api/friends/${req.query.id}`);
+
+      const id = { _id: ObjectId(req.query.id) };
+      const result = await db.collection(MONGODB_COLLECTION.FRIEND).findOne(id);
+
+      if (result != null) {
+        res.status(HTTP_STATUS_CODE.OK).json(result);
+      } else {
+        res.status(HTTP_STATUS_CODE.NOT_FOUND).json(result);
+      }
       break;
     }
     case HTTP.PATCH: {
+      logger.info(`HTTP PATCH: /api/friends/${req.query.id}`);
+
+      const id = { _id: ObjectId(req.query.id) };
+      const body = JSON.parse(JSON.stringify(req.body));
+
+      const result = await db
+        .collection(MONGODB_COLLECTION.FRIEND)
+        .replaceOne(id, body);
+
+      if (result.modifiedCount !== 0) {
+        res.status(HTTP_STATUS_CODE.OK).json(result);
+      } else {
+        res.status(HTTP_STATUS_CODE.NOT_FOUND).json(result);
+      }
       break;
     }
     case HTTP.DELETE: {
-      logger.info('HTTP DELETE: /api/friends/{ID}');
+      logger.info(`HTTP DELETE: /api/friends/${req.query.id}`);
+
       const id = { _id: ObjectId(req.query.id) };
       const result = await db
         .collection(MONGODB_COLLECTION.FRIEND)
         .deleteOne(id);
 
       if (result.deletedCount === 1) {
-        res.status(HTTP_STATUS_CODE.ACCEPTED).end(JSON.stringify(result));
+        res.status(HTTP_STATUS_CODE.ACCEPTED).json(result);
       } else {
-        res
-          .status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR)
-          .end(JSON.stringify(result));
+        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(result);
       }
-
       break;
     }
     default: {
+      logger.info('Invalid HTTP Method: /api/friends/{ID}');
+
       res.setHeader('Allow', [HTTP.GET, HTTP.PATCH, HTTP.DELETE]);
       res
         .status(HTTP_STATUS_CODE.METHOD_NOT_ALLOWED)
