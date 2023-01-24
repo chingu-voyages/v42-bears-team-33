7cceb5e16e41c77751d3b9237b1570db6449ba69
mirrer <alsejr1004@gmail.com>
2023-01-24 16:55:50 +0900

    Design: Landing page header style modify

--- front/style/landing/header.js
+++ front/style/landing/header.js
@@ -6,11 +6,13 @@ export const LandingWrapper = styled(Row)`
   padding: 6em 10em;
 
   @media ${({ theme }) => theme.media.tablet} {
-    padding: 3em 1em;
+    padding: 3em 4em;
   }
 `;
 
 export const LandingHeaderInfo = styled(Col)`
+  width: 50%;
+
   & > h1 {
     font-size: ${({ theme }) => theme.calcRem(38)};
     font-weight: 700;
@@ -23,7 +25,7 @@ export const LandingHeaderInfo = styled(Col)`
 
   @media ${({ theme }) => theme.media.tablet} {
     & > h1 {
-      font-size: ${({ theme }) => theme.calcRem(15)};
+      font-size: ${({ theme }) => theme.calcRem(12)};
       font-weight: 700;
     }
 
@@ -33,6 +35,7 @@ export const LandingHeaderInfo = styled(Col)`
 
     & > img {
       width: 13em;
+      margin: 0 auto;
     }
   }
 `;
