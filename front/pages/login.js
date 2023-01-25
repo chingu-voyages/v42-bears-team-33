3e659d8edde08ecc7534fd1f7e957187e6faad3e
mirrer <alsejr1004@gmail.com>
2023-01-25 14:24:27 +0900

    Feat: Minimal login function

--- front/pages/login.js
+++ front/pages/login.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'antd';
+import Router from 'next/router';
 import Head from 'next/head';
 
 import AppLayout from '@components/AppLayout';
+import { LOG_IN_SUCCESS } from '@reducers/user';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const { me } = useSelector(state => state.user);
+
+  const onClickLogin = useCallback(() => {
+    dispatch(LOG_IN_SUCCESS());
+  }, []);
+
+  useEffect(() => {
+    if (me) Router.push('/friends');
+  }, [me]);
+
   return (
     <>
       <Head>
@@ -12,6 +27,7 @@ const Login = () => {
 
       <AppLayout>
         <div>Login Page</div>
+        <Button onClick={onClickLogin}>Log in</Button>
       </AppLayout>
     </>
   );
--- front/reducers/user.js
+++ front/reducers/user.js
@@ -4,10 +4,22 @@ const initialState = {
   me: null,
 };
 
+const dummyUser = {
+  id: 1,
+  email: 'dummy@google.com',
+  nickname: 'Jenny Wilson',
+  profileImage: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+};
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    LOG_IN_SUCCESS: state => {
+      state.me = dummyUser;
+    },
+  },
 });
 
+export const { LOG_IN_SUCCESS } = userSlice.actions;
 export default userSlice.reducer;
